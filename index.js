53f72788cf1551e20e910c3fcec6dcd3916a1f25
Jupri Naldi <134026726+acil23@users.noreply.github.com>
2025-06-12 20:05:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ import predictRoutes from './routes/predict.js';
 import historyRoutes from './routes/history.js';
 import usersRoutes from './routes/users.js';
 
-const server = Hapi.server({
-  port: process.env.PORT || 3001,
-  host: '0.0.0.0', // penting untuk Railway!
-  routes: {
-    cors: {
-      origin: ['*'], // masih OK untuk testing, bisa diatur nanti
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT || 3001,
+    host: "0.0.0.0", // penting untuk Railway!
+    routes: {
+      cors: {
+        origin: ["*"], // masih OK untuk testing, bisa diatur nanti
+      },
     },
-  },
-});
+  });
 
 
   server.route([...usersRoutes, ...historyRoutes]);
